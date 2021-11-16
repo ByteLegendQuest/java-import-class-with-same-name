e15ce0630a5354078ac44e6d17624b14b6c456e4
baiwenbin <3409352279@qq.com>
2021-11-16 10:12:35 +0800

    fix(Home): Import Same Name Class

--- src/main/java/com/bytelegend/Home.java
+++ src/main/java/com/bytelegend/Home.java
@@ -1,9 +1,11 @@
 package com.bytelegend;
 
-import com.bytelegend.pet1.Cat;
-import com.bytelegend.pet2.Cat;
-
+/**
+ * @author Bai
+ * @version 1.0
+ * @since 2021.11.16
+ */
 public class Home {
-    Cat cat1;
-    Cat cat2;
+    com.bytelegend.pet1.Cat cat1;
+    com.bytelegend.pet2.Cat cat2;
 }
