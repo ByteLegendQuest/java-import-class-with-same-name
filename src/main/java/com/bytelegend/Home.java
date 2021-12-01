8a54b9e2befc4632de55a8e1a517f96e587341d4
lemon-altxx <2515782282@qq.com>
2021-12-01 06:23:18 +0000

    Update at 2021-12-01T06:23:18.243820Z

--- src/main/java/com/bytelegend/Home.java
+++ src/main/java/com/bytelegend/Home.java
@@ -1,8 +1,7 @@
 package com.bytelegend;
 
-import com.bytelegend.pet2.Cat;
-
+import com.bytelegend.pet1.Cat;
 public class Home {
-    com.bytelegend.pet1.Cat cat1;
-    Cat cat2;
+    Cat cat1;
+    com.bytelegend.pet2.Cat cat2;
 }
