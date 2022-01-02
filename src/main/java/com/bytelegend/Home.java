1c58d551670c19da2c8ba5d6ff17ae4ff0422b7c
mondler <mondler0418@yahoo.com>
2022-01-02 04:25:31 +0000

    Update at 2022-01-02T04:25:31.179111Z

--- src/main/java/com/bytelegend/Home.java
+++ src/main/java/com/bytelegend/Home.java
@@ -4,6 +4,6 @@ import com.bytelegend.pet1.Cat;
 import com.bytelegend.pet2.Cat;
 
 public class Home {
-    Cat cat1;
-    Cat cat2;
+    Cat cat1 = new com.bytelegend.pet1.Cat();
+    Cat cat2 = new com.bytelegend.pet1.Cat();
 }
