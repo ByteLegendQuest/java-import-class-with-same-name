b1d8192675459c15941332a6212b044205a160b1
gs20000521 <1154916494@qq.com>
2022-01-11 04:06:17 +0000

    Update at 2022-01-11T04:06:17.848808Z

--- src/main/java/com/bytelegend/Home.java
+++ src/main/java/com/bytelegend/Home.java
@@ -1,9 +1,9 @@
 package com.bytelegend;
 
-import com.bytelegend.pet1.Cat;
+import com.bytelegend.pet1.*;
 import com.bytelegend.pet2.Cat;
 
 public class Home {
-    public Cat cat1;
-    public Cat cat2;
+    Cat cat1;
+    Cat cat2;
 }
