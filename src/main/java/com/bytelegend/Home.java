501208c33efe9e03d02a251fee945c1182c798a2
gaozhidong <1011284295@qq.com>
2022-01-11 09:51:58 +0000

    Update at 2022-01-11T09:51:58.308492Z

--- src/main/java/com/bytelegend/Home.java
+++ src/main/java/com/bytelegend/Home.java
@@ -4,7 +4,6 @@ import com.bytelegend.pet1.Cat;
 import com.bytelegend.pet2.Cat;
 
 public class Home {
-    Cat cat1 = new com.bytelegend.pet1.Cat();
-    Cat cat2 = new com.bytelegend.pet2.Cat();
-
+    Cat cat1;
+    com.bytelegend.pet2.Cat cat2;
 }
