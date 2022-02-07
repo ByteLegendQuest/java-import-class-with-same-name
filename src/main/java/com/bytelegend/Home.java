44c08862b7738fb0c3e89cdd315acebf7fba155d
Angels-D <dyy3688098QQ@163.com>
2022-02-07 07:16:34 +0000

    Update at 2022-02-07T07:16:34.816110Z

--- src/main/java/com/bytelegend/Home.java
+++ src/main/java/com/bytelegend/Home.java
@@ -2,5 +2,5 @@ package com.bytelegend;
 
 public class Home {
     com.bytelegend.pet1.Cat cat1 = new com.bytelegend.pet1.Cat();
-    com.bytelegend.pet2.Cat cat1 = new com.bytelegend.pet2.Cat();
+    com.bytelegend.pet2.Cat cat2 = new com.bytelegend.pet2.Cat();
 }
