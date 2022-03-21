8ef30c5742c60f74ea299e7088a65a52401322ea
wl212 <2568247954@qq.com>
2022-03-21 02:31:26 +0000

    Update at 2022-03-21T02:31:26.836865Z

--- src/main/java/com/bytelegend/Home.java
+++ src/main/java/com/bytelegend/Home.java
@@ -4,7 +4,10 @@ import com.bytelegend.pet1.Cat;
 import com.bytelegend.pet2.Cat;
 
 public class Home {
-     home.cat1 = new com.bytelegend.pet1.Cat();
+    public void homeHasTwoKindsOfCat() {
+        Home home = new Home();
+        home.cat1 = new com.bytelegend.pet1.Cat();
         home.cat2 = new com.bytelegend.pet2.Cat();
-
+    }
 }
+ 
