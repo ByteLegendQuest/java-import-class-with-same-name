fa61aa455df9641c0c64bcfdf2b4837fa16791d1
Ozge13 <ozge.ozdes@avisi.nl>
2022-03-21 21:06:32 +0000

    Update at 2022-03-21T21:06:32.246607Z

--- src/main/java/com/bytelegend/Home.java
+++ src/main/java/com/bytelegend/Home.java
@@ -1,9 +1,12 @@
 package com.bytelegend;
 
-import com.bytelegend.pet1.Cat;
-import com.bytelegend.pet2.Cat;
+import org.junit.jupiter.api.Test;
 
-public class Home {
-    Cat cat1;
-    Cat cat2;
+public class HomeTest {
+    @Test
+    public void homeHasTwoKindsOfCat() {
+        Home home = new Home();
+        home.cat1 = new com.bytelegend.pet1.Cat();
+        home.cat2 = new com.bytelegend.pet1.Cat();
+    }
 }
