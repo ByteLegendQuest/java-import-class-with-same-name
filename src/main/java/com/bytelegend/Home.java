17341850b07b1a46637ec37448a5cd42554d2fed
michealmachine <micheal0machine@gmail.com>
2022-03-24 01:50:34 +0000

    Update at 2022-03-24T01:50:34.421771Z

--- src/main/java/com/bytelegend/Home.java
+++ src/main/java/com/bytelegend/Home.java
@@ -4,7 +4,7 @@ import com.bytelegend.pet1.Cat;
 import com.bytelegend.pet2.Cat;
 
 public class Home {
-    Cat.pet1.Cat cat1;
-    Cat.pet2.Cat cat2;
+    Cat.com.bytelegend.pet1.Cat cat1;
+    Cat.com.bytelegend.pet2.Cat cat2;
     
 }
