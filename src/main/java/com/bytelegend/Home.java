3c56a8ec35768eae6858a599ac6862f0f5a0ecb7
Laura-bf <laurabf6@gmail.com>
2022-03-29 13:52:35 +0000

    Update at 2022-03-29T13:52:35.705973Z

--- src/main/java/com/bytelegend/Home.java
+++ src/main/java/com/bytelegend/Home.java
@@ -1,8 +1,11 @@
 package com.bytelegend;
 
-import com.bytelegend.*.Cat;
+import com.bytelegend.pet1.Cat;
+import com.bytelegend.pet2.Cat;
 
 public class Home {
     Cat cat1;
     Cat cat2;
+
+    cat1=cat2;
 }
