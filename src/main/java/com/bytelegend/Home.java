f52b51136adae6af6360f0708ee6a45b4c5f60b4
1earnerr <1908663097@qq.com>
2022-03-30 08:56:17 +0000

    Update at 2022-03-30T08:56:17.264810Z

--- src/main/java/com/bytelegend/Home.java
+++ src/main/java/com/bytelegend/Home.java
@@ -4,6 +4,6 @@ import com.bytelegend.pet1.Cat;
 import com.bytelegend.pet2.Cat;
 
 public class Home {
-    public Cat cat1;
-    public Cat cat2;
+    public pet1.Cat cat1;
+    public pet2.Cat cat2;
 }
