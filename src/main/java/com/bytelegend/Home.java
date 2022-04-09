66502a2383c67d672344b5a0f67a7f39894d58b1
1994xiong <xiongmengxm@163.com>
2022-04-09 08:10:28 +0000

    Update at 2022-04-09T08:10:28.507486Z

--- src/main/java/com/bytelegend/Home.java
+++ src/main/java/com/bytelegend/Home.java
@@ -1,9 +1,9 @@
 package com.bytelegend;
 
-import com.bytelegend.pet1.*;
-import com.bytelegend.pet2.*;
+import com.bytelegend.pet1.Cat;
+import com.bytelegend.pet2.Cat;
 
 public class Home {
-    Cat cat1;
+    com.bytelegend.pet1.Cat cat1;
     com.bytelegend.pet2.Cat car2;
 }
