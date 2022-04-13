316e9cacd5eeba305ae6758f51a7ff941010d30d
kishilong <2521152053@qq.com>
2022-04-13 12:43:39 +0000

    Update at 2022-04-13T12:43:39.028570Z

--- src/main/java/com/bytelegend/Home.java
+++ src/main/java/com/bytelegend/Home.java
@@ -1,6 +1,8 @@
 package com.bytelegend;
 
-from a import com.bytelegend.pet1.Cat as com.bytelegend.pet2.Cat;
+import com.bytelegend.pet1.Cat;
+import com.bytelegend.pet2.Cat;
+
 
 public class Home {
     Cat cat1;
