581010c1905e558130869977a632561234cf91d5
Azhigit <Azgithub@163.com>
2022-04-19 03:02:30 +0000

    Update at 2022-04-19T03:02:30.651843Z

--- src/main/java/com/bytelegend/Home.java
+++ src/main/java/com/bytelegend/Home.java
@@ -4,6 +4,9 @@ import com.bytelegend.pet1.Cat;
 import com.bytelegend.pet2.Cat;
 
 public class Home {
+    public vstatic void main(String[] args){
     Cat cat1 = new com.bytelegend.pet1.Cat();
     Cat cat2 = new com.bytelegend.pet2.Cat();
+    
+    }
 }
