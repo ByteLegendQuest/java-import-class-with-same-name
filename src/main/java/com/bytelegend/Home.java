621a37d0df798f77f07c7ed69c96dae92fbe9514
Azhigit <Azgithub@163.com>
2022-04-19 02:46:47 +0000

    Update at 2022-04-19T02:46:47.180466Z

--- src/main/java/com/bytelegend/Home.java
+++ src/main/java/com/bytelegend/Home.java
@@ -4,6 +4,7 @@ import com.bytelegend.pet1.Cat;
 import com.bytelegend.pet2.Cat;
 
 public class Home {
-    Cat cat1;
-    Cat cat2;
+    Home home = new Home();
+    home.cat1 =  com.bytelegend.pet1.Cat;
+    home.cat2 =  com.bytelegend.pet2.Cat;
 }
