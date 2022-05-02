ca8dce54ecbe5fa8c640d9b5350c2bd739314aaf
Super-XiaoNian <1962971038@qq.com>
2022-05-02 03:03:20 +0000

    Update at 2022-05-02T03:03:20.840180Z

--- src/main/java/com/bytelegend/Home.java
+++ src/main/java/com/bytelegend/Home.java
@@ -2,6 +2,6 @@ package com.bytelegend;
 
 
 public class Home {
-    com.bytelegend.pet1.Cat Cat1=new com.bytelegend.pet1.Cat;
-    com.bytelegend.pet2.Cat Cat2=new com.bytelegend.pet2.Cat;
+    com.bytelegend.pet1.Cat Cat1=new com.bytelegend.pet1.Cat();
+    com.bytelegend.pet2.Cat Cat2=new com.bytelegend.pet2.Cat();
 }
