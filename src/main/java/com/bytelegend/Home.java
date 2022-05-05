06cec0fb8e4613b54019301f8b0bbc5681f6fbe2
tristean1 <danieltristean@gmail.com>
2022-05-05 05:40:33 +0000

    Update at 2022-05-05T05:40:33.545691Z

--- src/main/java/com/bytelegend/Home.java
+++ src/main/java/com/bytelegend/Home.java
@@ -1,9 +1,9 @@
 package com.bytelegend;
 
-import main.java.com.bytelegend.pet1.Cat;
-import main.java.com.bytelegend.pet2.Cat;
+import com.bytelegend.pet1.Cat;
+import com.bytelegend.pet2.Cat;
 
 public class Home {
-    main.java.com.bytelegend.pet1.Cat cat1;
-    main.java.com.bytelegend.pet2.Cat cat2;
+    Cat cat1;
+    Cat cat2;
 }
