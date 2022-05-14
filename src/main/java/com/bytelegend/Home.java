64270bb93da17fe03cde484d8d907d99f07a768c
drnarika <chenbendan2@163.com>
2022-05-14 11:35:19 +0000

    Update at 2022-05-14T11:35:19.462760Z

--- src/main/java/com/bytelegend/Home.java
+++ src/main/java/com/bytelegend/Home.java
@@ -1,9 +1,9 @@
 package com.bytelegend;
 
-import com.bytelegend.pet1.Cat;
-import com.bytelegend.pet2.Cat;
+import com.bytelegend.pet1.*;
+import com.bytelegend.pet2.*;
 
 public class Home {
-    pet1.Cat cat1;
-    pet2.Cat cat2;
+    com.bytelegend.pet1.Cat cat1;
+    com.bytelegend.pet2.Cat cat2;
 }
