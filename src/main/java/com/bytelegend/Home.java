24e4915e7b63834fd351c9efd5b2432b3343116c
SpaceTimeAdministrator <106767177+SpaceTimeAdministrator@users.noreply.github.com>
2022-06-04 06:03:20 +0000

    Update at 2022-06-04T06:03:20.546486Z

--- src/main/java/com/bytelegend/Home.java
+++ src/main/java/com/bytelegend/Home.java
@@ -1,9 +1,10 @@
 package com.bytelegend;
 
 import com.bytelegend.pet1.Cat;
-import com.bytelegend.pet2.Cat;
+import com.bytelegend.pet2.*;
 
 public class Home {
-    Cat cat1;
-    Cat cat2;
+    Cat cat1 = new Cat();
+    com.bytelegend.pet2.Cat cat2 = new com.bytelegend.pet2.Cat();
+    
 }
