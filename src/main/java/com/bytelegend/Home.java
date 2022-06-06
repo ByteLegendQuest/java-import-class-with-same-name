fd7893f93787adfbfc6a8027caa9bd675b59439c
GergertHatby <yanghao9225@outlook.com>
2022-06-06 12:33:24 +0000

    Update at 2022-06-06T12:33:24.024029Z

--- src/main/java/com/bytelegend/Home.java
+++ src/main/java/com/bytelegend/Home.java
@@ -1,11 +1,5 @@
 package com.bytelegend;
-
-
-import com.bytelegend.pet1.Cat;
-
-
 public class Home {
-    Cat a =new Cat();
     com.bytelegend.pet2.Cat  cat =new com.bytelegend.pet2.Cat();
-
+    com.bytelegend.pet1.Cat  cat2 =new com.bytelegend.pet1.Cat();
 }
