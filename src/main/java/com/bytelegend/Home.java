07c4b3c93500d7640083801e6fc3eb41132c14a6
wl212 <2568247954@qq.com>
2022-06-14 13:57:44 +0000

    Update at 2022-06-14T13:57:44.422389Z

--- src/main/java/com/bytelegend/Home.java
+++ src/main/java/com/bytelegend/Home.java
@@ -1,10 +1,8 @@
-package com.bytelegend.pet1;
-package com.bytelegend.pet2;
 
 import com.bytelegend.pet1.Cat;
 import com.bytelegend.pet2.Cat;
 
 public class Home {
-    Cat cat1;
-    Cat cat2;
+   com.bytelegend.pet1.Cat cat1;
+   com.bytelegend.pet1.Cat cat2;
 }
