2d3d56a072c9f3606423bfc2fc0a4abff315119d
wl212 <2568247954@qq.com>
2022-06-14 14:07:45 +0000

    Update at 2022-06-14T14:07:45.806480Z

--- src/main/java/com/bytelegend/Home.java
+++ src/main/java/com/bytelegend/Home.java
@@ -1,7 +1,9 @@
+package com.bytelegend.pet1;
+package com.bytelegend.pet2;
 
 
 
 public class Home {
-   com.bytelegend.pet1.Cat cat1;
-   com.bytelegend.pet1.Cat.cat2;
+    Cat cat1;
+    Cat cat2;
 }
