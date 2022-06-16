7115b11a7fcb2daaf1a35be852c0f3ea2d857582
karolk75 <karolk410410@gmail.com>
2022-06-16 11:31:49 +0000

    Update at 2022-06-16T11:31:49.090008Z

--- src/main/java/com/bytelegend/Home.java
+++ src/main/java/com/bytelegend/Home.java
@@ -1,7 +1,6 @@
 package com.bytelegend;
 
-import com.bytelegend.pet1.Cat;
-import com.bytelegend.pet2.Cat;
+import com.bytelegend.*;
 
 public class Home {
     com.bytelegend.pet1.Cat cat1 = new com.bytelegend.pet1.Cat();
