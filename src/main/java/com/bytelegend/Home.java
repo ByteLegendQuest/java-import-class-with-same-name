a3cd99e2cf555cceba201f0d8440b4a5d5edb2f3
Kaizokii <yelengyang@gmail.com>
2022-07-01 01:59:06 +0000

    Update at 2022-07-01T01:59:06.230817Z

--- src/main/java/com/bytelegend/Home.java
+++ src/main/java/com/bytelegend/Home.java
@@ -1,10 +1,13 @@
 package com.bytelegend;
 
-import com.bytelegend.pet1.Cat;
+import com.bytelegend.pet1.Cat;;
 
 public class Home {
     public static void main(String args[]) {
         new Cat();
         new com.bytelegend.pet2.Cat();
     }
+
+    public Cat cat1;
+    public com.bytelegend.pet2.Cat cat2;
 }
