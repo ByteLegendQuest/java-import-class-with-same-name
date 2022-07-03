67d0e5fef7364350a4129de4df16954bf9cc8f49
HowardY2 <781600827@qq.com>
2022-07-03 02:32:55 +0000

    Update at 2022-07-03T02:32:55.630941Z

--- src/main/java/com/bytelegend/Home.java
+++ src/main/java/com/bytelegend/Home.java
@@ -1,9 +1,8 @@
 package com.bytelegend;
 
-import com.bytelegend.pet1;
-import com.bytelegend.pet2;
+import com.bytelegend.pet1.Cat;
 
 public class Home {
-    pet1.Cat cat1;
-    pet2.Cat cat2;
+    Cat cat1;
+    com.bytelegend.pet2.Cat cat2;
 }
