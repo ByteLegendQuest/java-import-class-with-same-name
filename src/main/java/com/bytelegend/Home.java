0b4ec2447b34417b8fc17cf2290f7e5aa6af6832
Birlinha <omenaribeiro13@gmail.com>
2022-07-11 01:33:02 +0000

    Update at 2022-07-11T01:33:02.048050Z

--- src/main/java/com/bytelegend/Home.java
+++ src/main/java/com/bytelegend/Home.java
@@ -1,9 +1,8 @@
 package com.bytelegend;
 
 import com.bytelegend.pet1.Cat;
-import com.bytelegend.pet2.Cat;
 
 public class Home {
-    public pet1.Cat cat1;
-    public pet2.Cat cat2;
+    public Cat cat1;
+    public com.bytelegend.pet2.Cat cat2;
 }
