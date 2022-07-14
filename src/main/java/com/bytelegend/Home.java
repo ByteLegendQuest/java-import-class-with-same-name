918a5c10d882f6dd63636b79aec5f2a670565b26
tassellll <2112562745@qq.com>
2022-07-14 00:01:43 +0000

    Update at 2022-07-14T00:01:43.015546Z

--- src/main/java/com/bytelegend/Home.java
+++ src/main/java/com/bytelegend/Home.java
@@ -1,8 +1,5 @@
 package com.bytelegend;
 
-import com.bytelegend.pet1.Cat;
-import com.bytelegend.pet2.Cat;
-
 public class Home {
     com.bytelegend.pet1.Cat cat1;
     com.bytelegend.pet2.Cat cat2;
