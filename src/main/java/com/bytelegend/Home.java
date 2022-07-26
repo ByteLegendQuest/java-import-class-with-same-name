83106aa8ce0d5c5a4103bb40ab9bd09912d34c6c
Jiu-Notte <284727826@qq.com>
2022-07-26 04:11:26 +0000

    Update at 2022-07-26T04:11:26.730Z

--- src/main/java/com/bytelegend/Home.java
+++ src/main/java/com/bytelegend/Home.java
@@ -6,6 +6,6 @@ import com.bytelegend.pet2.Cat;
 public class Home {
     Cat cat1;
     Cat cat2;
-    com.bytelegend.pet1.Cat cat1;
-    com.bytelegend.pet2.Cat cat2;
+    pet1.Cat cat1;
+    pet2.Cat cat2;
 }
