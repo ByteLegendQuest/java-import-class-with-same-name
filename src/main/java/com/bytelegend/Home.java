7df64bd224d18fc0ca400f39dba35fa6b568e348
hibisicus <1772527675@qq.com>
2022-08-10 15:17:47 +0000

    Update at 2022-08-10T15:17:47.756390Z

--- src/main/java/com/bytelegend/Home.java
+++ src/main/java/com/bytelegend/Home.java
@@ -3,7 +3,7 @@ package com.bytelegend;
 import com.bytelegend.pet1.Cat;
 import com.bytelegend.pet2.Cat;
 
-public class Home {
-    Cat cat1 = new com.bytelegend.pet1.Cat();
-    Cat cat2 = new com.bytelegend.pet2.Cat();
+public class Home {    
+    com.bytelegend.pet1.Cat cat1 = new com.bytelegend.pet1.Cat();
+    com.bytelegend.pet2.Cat cat2 = new com.bytelegend.pet2.Cat();
 }
