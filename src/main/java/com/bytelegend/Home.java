fcca86f84ba74683d7ae755926992df243ec60af
nwomn <rongziqian5@gmail.com>
2022-08-11 13:19:45 +0000

    Update at 2022-08-11T13:19:45.273245Z

--- src/main/java/com/bytelegend/Home.java
+++ src/main/java/com/bytelegend/Home.java
@@ -1,9 +1,13 @@
 package com.bytelegend;
 
-import com.bytelegend.pet1.Cat1;
-import com.bytelegend.pet2.Cat2;
+import com.bytelegend.pet1.Cat;
 
 public class Home {
-    public Cat1 cat1;
-    public Cat2 cat2;
+    Cat cat1;
+}
+
+import com.bytelegend.pet2.Cat;
+
+public class Home {
+    Cat cat2;
 }
