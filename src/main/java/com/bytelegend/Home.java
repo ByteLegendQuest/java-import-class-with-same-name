9ed119053d51832bf1321b6204bede2bf14e94f9
lulovelu <2972521663@qq.com>
2022-09-07 12:09:29 +0000

    Update at 2022-09-07T12:09:29.256325Z

--- src/main/java/com/bytelegend/Home.java
+++ src/main/java/com/bytelegend/Home.java
@@ -4,6 +4,6 @@ import com.bytelegend.pet1.Cat;
 import com.bytelegend.pet2.Cat;
 
 public class Home {
-    Cat cat1;
-    Cat cat2;
+    com.bytelegend.pet1.Cat.Cat cat1;
+    com.bytelegend.pet2.Cat.Cat cat2;
 }
