28319fc996fd9a8f6946e3c40e5ad2a0596a1e82
an5er <1318861442@qq.com>
2022-09-21 01:03:12 +0000

    Update at 2022-09-21T01:03:12.564259Z

--- src/main/java/com/bytelegend/Home.java
+++ src/main/java/com/bytelegend/Home.java
@@ -4,6 +4,6 @@ import com.bytelegend.pet1.Cat;
 import com.bytelegend.pet2.Cat;
 
 public class Home {
-    Cat cat1;
-    Cat cat2;
+    Cat com.bytelegend.pet1.Cat;
+    Cat com.bytelegend.pet2.Cat;
 }
