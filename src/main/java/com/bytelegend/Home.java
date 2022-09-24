8c0a678d261e5629256ed1728be0bb5a9de7bf68
Zeon3 <1366685013@qq.com>
2022-09-24 09:57:01 +0000

    Update at 2022-09-24T09:57:01.701915Z

--- src/main/java/com/bytelegend/Home.java
+++ src/main/java/com/bytelegend/Home.java
@@ -3,7 +3,7 @@ package com.bytelegend;
 import com.bytelegend.pet1.Cat;
 import com.bytelegend.pet2.Cat;
 
-public class Home extends Cat{
-    Cat cat1;
-    Cat cat2;
+public class Home {
+    Cat cat1 = new Home();
+    Cat cat2 = new Home();
 }
