e01f0d5647452944f8b3d3269bcebc740bc49e6b
Zeon3 <1366685013@qq.com>
2022-09-24 09:50:07 +0000

    Update at 2022-09-24T09:50:07.022045Z

--- src/main/java/com/bytelegend/Home.java
+++ src/main/java/com/bytelegend/Home.java
@@ -4,6 +4,6 @@ import com.bytelegend.pet1.Cat;
 import com.bytelegend.pet2.Cat;
 
 public class Home {
-    Cat cat1 = new Cat();
-    Cat cat2 = new Cat();
+    pet1.Cat cat1;
+    pet2.Cat cat2;
 }
