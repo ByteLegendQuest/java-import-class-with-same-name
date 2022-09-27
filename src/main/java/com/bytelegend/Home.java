cc8e9cca45103a5963acde82f92e1221dca224ae
Qiqi7-7 <writetomars@qq.com>
2022-09-27 07:40:03 +0000

    Update at 2022-09-27T07:40:03.249864Z

--- src/main/java/com/bytelegend/Home.java
+++ src/main/java/com/bytelegend/Home.java
@@ -1,9 +1,6 @@
 package com.bytelegend;
 
-import com.bytelegend.pet1.Cat as Cat1;
-import com.bytelegend.pet2.Cat as Cat2;
-
 public class Home {
-    Cat1 cat1;
-    Cat2 cat2;
+    com.bytelegend.pet1.Cat cat1;
+    com.bytelegend.pet2.Cat cat2;
 }
