867aa8fd787a2606c764ec24f62a53dea202306f
tiger0961 <29515547@qq.com>
2022-10-10 08:33:50 +0000

    Update at 2022-10-10T08:33:50.699074Z

--- src/main/java/com/bytelegend/Home.java
+++ src/main/java/com/bytelegend/Home.java
@@ -1,9 +1,9 @@
 package com.bytelegend;
 
-import com.bytelegend.pet1.Cat;
-import com.bytelegend.pet2.Cat;
+import com.bytelegend.pet1.Cat as Cat1;
+import com.bytelegend.pet2.Cat as Cat2;
 
 public class Home {
-   pet1.Cat cat1;
-   pet2.Cat cat2;
+   Cat1 cat1;
+   Cat2 cat2;
 }
