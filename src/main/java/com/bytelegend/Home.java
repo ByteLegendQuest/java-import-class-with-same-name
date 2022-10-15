2967affc1d507747e02c921026f4365d3e63dd79
YJiva <2280134514@qq.com>
2022-10-15 12:15:59 +0000

    Update at 2022-10-15T12:15:59.867079Z

--- src/main/java/com/bytelegend/Home.java
+++ src/main/java/com/bytelegend/Home.java
@@ -1,9 +1,10 @@
 package com.bytelegend;
 
-import com.bytelegend.pet1.Cat;
-import com.bytelegend.pet2.Cat as AnotherCat;
+
+
+import com.bytelegend.pet2.Cat;
 
 public class Home {
-    Public Cat cat1;
-    Public AnotherCat cat2;
+    com.bytelegend.pet1.Cat cat1;
+    Cat cat2;
 }
