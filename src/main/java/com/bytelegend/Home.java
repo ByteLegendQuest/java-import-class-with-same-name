186ae09180d28b49e1423aefca53372c3d489d7d
1026021471 <1026021471@qq.com>
2022-11-09 07:09:25 +0000

    Update at 2022-11-09T07:09:25.222506Z

--- src/main/java/com/bytelegend/Home.java
+++ src/main/java/com/bytelegend/Home.java
@@ -1,11 +1,11 @@
 package com.bytelegend;
 
-import com.bytelegend.pet1.Cat;
+import com.bytelegend.pet1.*;
 import com.bytelegend.pet2.Cat;
 
 public class Home {
-    Cat cat1 = new com.bytelegend.pet1.Cat();
-    Cat cat2 = new com.bytelegend.pet2.Cat();
+    com.bytelegend.pet1.Cat cat1 = new com.bytelegend.pet1.Cat();
+    Cat cat2 = new Cat();
     public static void main(String[] args) {
     }
 }
