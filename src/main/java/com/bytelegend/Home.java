302cc2c05063a163e107fa6107bf48ee55b6be44
Newelle <wozi1988@proton.me>
2022-11-12 10:44:18 +0000

    Update at 2022-11-12T10:44:18.301809Z

--- src/main/java/com/bytelegend/Home.java
+++ src/main/java/com/bytelegend/Home.java
@@ -4,6 +4,6 @@ import com.bytelegend.pet1.Cat;
 import com.bytelegend.pet2.Cat;
 
 public class Home {
-    com.bytelegend.pet2.Cat cat2 = new com.bytelegend.pet2.Cat();
+    bytelegend.pet2.Cat cat2 = new bytelegend.pet2.Cat();
     Cat cat1 = new Cat();
 }
