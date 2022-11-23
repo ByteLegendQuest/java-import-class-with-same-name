2cec5d0250a50bbfbd83c017961ad9284638e19d
ljsalt <lj27128176@outlook.com>
2022-11-23 14:38:01 +0000

    Update at 2022-11-23T14:38:01.699919Z

--- src/main/java/com/bytelegend/Home.java
+++ src/main/java/com/bytelegend/Home.java
@@ -4,5 +4,5 @@ import com.bytelegend.pet2.Cat;
 
 public class Home {
 com.bytelegend.pet1.Cat cat1=new com.bytelegend.pet1.Cat();
-com.bytelegend.pet2.Cat cat1=new com.bytelegend.pet2.Cat();
+com.bytelegend.pet2.Cat cat2=new com.bytelegend.pet2.Cat();
 }
