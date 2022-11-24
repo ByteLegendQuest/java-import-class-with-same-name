70841143ecdbf1087a9cf7899c3a43ff151c8953
EverNewer <1723045846@qq.com>
2022-11-24 07:44:04 +0000

    Update at 2022-11-24T07:44:04.135537Z

--- src/main/java/com/bytelegend/Home.java
+++ src/main/java/com/bytelegend/Home.java
@@ -1,9 +1,9 @@
 package com.bytelegend;
 
 import com.bytelegend.pet1.Cat;
-import com.bytelegend.pet2.Cat;
+import com.bytelegend.pet2.*;
 
 public class Home {
-    com.bytelegend.pet1.Cat cat1 = new Cat();
-    com.bytelegend.pet2.Cat cat2 = new Cat();
+    com.bytelegend.pet1.Cat cat1 = new com.bytelegend.pet1.Cat();
+    com.bytelegend.pet2.Cat cat2 = new com.bytelegend.pet2.Cat();
 }
