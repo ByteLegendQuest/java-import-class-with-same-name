c3e566c5ea26eb2f90c0fdf6822ea1e546cd7a38
wusuoweiandjr <1047365005@qq.com>
2022-11-25 15:41:14 +0000

    Update at 2022-11-25T15:41:14.110367Z

--- src/main/java/com/bytelegend/Home.java
+++ src/main/java/com/bytelegend/Home.java
@@ -4,6 +4,6 @@ import com.bytelegend.pet1.Cat;
 import com.bytelegend.pet2.Cat;
 
 public class Home {
-   public Cat cat1;
-   public Cat cat2;
+    Cat cat1=new com.bytelegend.pet1.Cat();;
+    Cat cat2=new com.bytelegend.pet2.Cat();;
 }
