34548d8de6acb372b716945a4f78ab3df940df41
jenny-2233 <822204051@qq.com>
2022-11-26 07:30:56 +0000

    Update at 2022-11-26T07:30:56.741795Z

--- src/main/java/com/bytelegend/Home.java
+++ src/main/java/com/bytelegend/Home.java
@@ -4,7 +4,7 @@ import com.bytelegend.pet1.Cat;
 import com.bytelegend.pet2.Cat;
 
 public class Home {
-    Cat cat1 = new Cat();
-    Cat cat2 = new Cat();
+    Cat cat1 = new  com.bytelegend.pet1.Cat();
+    Cat cat2 = new  com.bytelegend.pet2.Cat();
     
 }
