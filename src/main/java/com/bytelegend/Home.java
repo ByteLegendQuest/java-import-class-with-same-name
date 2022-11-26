8b82dfeccdc9c1d4ef2fdc6578fe59bf54820eff
wusuoweiandjr <1047365005@qq.com>
2022-11-26 03:50:23 +0000

    Update at 2022-11-26T03:50:23.308817Z

--- src/main/java/com/bytelegend/Home.java
+++ src/main/java/com/bytelegend/Home.java
@@ -1,9 +1,9 @@
 package com.bytelegend;
 
-import main.java.com.bytelegend.pet1.*;
-import main.java.com.bytelegend.pet2.Cat;
+import com.bytelegend.pet1.Cat;
+import com.bytelegend.pet2.Cat;
 
 public class Home {
-   main.java.com.bytelegend.pet1.Cat cat1=new main.java.com.bytelegend.pet1.Cat();
-   main.java.com.bytelegend.pet2.Cat cat2=new main.java.com.bytelegend.pet2.Cat();
+  com.bytelegend.pet2.Cat cat1;
+  com.bytelegend.pet2.Cat cat2;
 }
