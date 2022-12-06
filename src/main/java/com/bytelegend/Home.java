ec5eac10c87b299a09dc16bd90fa18e248ce7754
ChaosBeiduo <1801764634@qq.com>
2022-12-06 01:38:00 +0000

    Update at 2022-12-06T01:38:00.621880Z

--- src/main/java/com/bytelegend/Home.java
+++ src/main/java/com/bytelegend/Home.java
@@ -1,7 +1,9 @@
 package com.bytelegend;
 
 public class Home {
-    home = new Home();
-    home.cat1 = new com.bytelegend.pet1.Cat();
-    home.cat2 = new com.bytelegend.pet2.Cat();
+    public static void main(String[] args){
+        home = new Home();
+        home.cat1 = new com.bytelegend.pet1.Cat();
+        home.cat2 = new com.bytelegend.pet2.Cat();
+    }
 }
