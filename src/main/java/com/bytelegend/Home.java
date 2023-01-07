9f656232131296667e558fa0b006ab086df54220
YoungerKayn <79000036+YoungerKayn@users.noreply.github.com>
2023-01-07 04:47:37 +0000

    Update at 2023-01-07T04:47:37.204781Z

--- src/main/java/com/bytelegend/Home.java
+++ src/main/java/com/bytelegend/Home.java
@@ -1,6 +1,6 @@
 package com.bytelegend;
 
 public class Home {
-    Cat cat1 = new com.bytelegend.pet1.Cat();
-    Cat cat2 = new com.bytelegend.pet2.Cat();
+    com.bytelegend.pet1.Cat cat1 = new com.bytelegend.pet1.Cat();
+    com.bytelegend.pet2.Cat cat2 = new com.bytelegend.pet2.Cat();
 }
