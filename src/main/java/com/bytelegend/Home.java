738033073ac0c233b5282c751523bd4c42eba48c
wangdeguxiang <514435906@qq.com>
2023-01-15 10:35:50 +0000

    Update at 2023-01-15T10:35:50.021721Z

--- src/main/java/com/bytelegend/Home.java
+++ src/main/java/com/bytelegend/Home.java
@@ -1,9 +1,9 @@
 package com.bytelegend;
 
 import com.bytelegend.pet1.Cat;
-import com.bytelegend.pet1.Cat;
+import com.bytelegend.pet2.Cat1;
 
 public class Home {
     Cat cat1;
-    Cat cat2;
+    Cat1 cat2;
 }
