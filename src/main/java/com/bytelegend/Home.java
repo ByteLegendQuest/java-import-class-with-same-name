23d8ec8ca84fb369b43c99c3edc60e18153cf9e6
wxcws <68136156@qq.com>
2023-02-14 06:01:34 +0000

    Update at 2023-02-14T06:01:34.165649Z

--- src/main/java/com/bytelegend/Home.java
+++ src/main/java/com/bytelegend/Home.java
@@ -4,6 +4,7 @@ import com.bytelegend.pet1.Cat;
 import com.bytelegend.pet2.Cat;
 
 public class Home {
-    public com.bytelegend.pet1.Cat cat;
-    public com.bytelegend.pet2.Cat cat;
+    Home home = new Home();
+    home.cat1 = new com.bytelegend.pet1.Cat();
+    home.cat2 = new com.bytelegend.pet2.Cat();
 }
