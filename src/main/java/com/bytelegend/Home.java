cc3475e60c204e4d4bd930679303207dddd15b64
wxcws <68136156@qq.com>
2023-02-14 05:43:43 +0000

    Update at 2023-02-14T05:43:43.975529Z

--- src/main/java/com/bytelegend/Home.java
+++ src/main/java/com/bytelegend/Home.java
@@ -4,6 +4,6 @@ import com.bytelegend.pet1.Cat;
 import com.bytelegend.pet2.Cat;
 
 public class Home {
-    public com.bytelegend.pet1.Cat cat1;
-    public com.bytelegend.pet2.Cat cat2;
+    public com.bytelegend.pet1.Cat cat;
+    public com.bytelegend.pet2.Cat cat;
 }
