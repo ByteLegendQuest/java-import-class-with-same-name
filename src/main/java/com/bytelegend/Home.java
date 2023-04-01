946716ce15b725bdca62b54da948aad310f48632
HumoWong <humeanity@163.com>
2023-04-01 16:13:10 +0000

    Update at 2023-04-01T16:13:10.665891Z

--- src/main/java/com/bytelegend/Home.java
+++ src/main/java/com/bytelegend/Home.java
@@ -4,6 +4,6 @@ import com.bytelegend.pet1.Cat;
 import com.bytelegend.pet2.*;
 
 public class Home {
-    Cat com.bytelegend.pet1.Cat;
-    Cat com.bytelegend.pet2.Cat;
+    Cat cat1;
+    Cat cat2;
 }
