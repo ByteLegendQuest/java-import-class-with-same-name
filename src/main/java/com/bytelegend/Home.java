0ae6663e7aaf9a1cd9ea999e6d3e773fe330f424
Becase <happiness0911@126.com>
2023-04-02 07:20:05 +0000

    Update at 2023-04-02T07:20:05.405295Z

--- src/main/java/com/bytelegend/Home.java
+++ src/main/java/com/bytelegend/Home.java
@@ -1,9 +1,9 @@
 package com.bytelegend;
 
-import com.bytelegend.pet1.Cat as Cat1;
-import com.bytelegend.pet2.Cat as Cat2;
+import com.bytelegend.pet1.Cat;
+import com.bytelegend.pet2.*;
 
 public class Home {
-    Cat1 cat1;
-    Cat1 cat2;
+    Cat cat1;
+    pet2.Cat cat2;
 }
