ce3322e9f09baf5475d8b0ef4a7eddc54d8543a3
RyouSY <731867416@qq.com>
2023-05-14 11:12:01 +0000

    Update at 2023-05-14T11:12:01.364645Z

--- src/main/java/com/bytelegend/Home.java
+++ src/main/java/com/bytelegend/Home.java
@@ -4,6 +4,6 @@ import com.bytelegend.pet1.Cat;
 import com.bytelegend.pet2.Cat;
 
 public class Home {
-   public cat1 = new com.bytelegend.pet1.Cat();
-   public cat2 = new com.bytelegend.pet2.Cat();
+   cat1 = new com.bytelegend.pet1.Cat();
+   cat2 = new com.bytelegend.pet2.Cat();
 }
