9b4eae2a67c4ba222f90bc532435d1b316a35c1c
rosettastoned12 <officiallynabiel@gmail.com>
2023-05-20 15:31:09 +0000

    Update at 2023-05-20T15:31:09.441995Z

--- src/main/java/com/bytelegend/Home.java
+++ src/main/java/com/bytelegend/Home.java
@@ -4,7 +4,11 @@ import com.bytelegend.pet1.Cat;
 import com.bytelegend.pet2.Cat;
 
 public class Home {
-    Cat cat1 = new com.bytelegend.pet1.Cat();
-    Cat cat2 = new com.bytelegend.pet2.Cat();
-    
+    Cat cat1;
+    Cat cat2;
+    public void hasTwoCats {
+        Home home = new Home();
+        home.cat1 = new com.bytelegend.pet1.Cat()
+        home.cat2 = new com.bytelegend.pet2.Cat()
+    }
 }
