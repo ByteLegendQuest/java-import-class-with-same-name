bbb1e5c86b0130562f59efc5e977a25144422ae8
jur4 <jramm88@web.de>
2023-05-24 18:58:53 +0000

    Update at 2023-05-24T18:58:53.984510Z

--- src/main/java/com/bytelegend/Home.java
+++ src/main/java/com/bytelegend/Home.java
@@ -1,9 +1,9 @@
 package com.bytelegend;
 
 import com.bytelegend.pet1.Cat;
-import com.bytelegend.pet2.Cat;
+import com.bytelegend.pet2.Cat as Cat2;
 
 public class Home {
     Cat cat1;
-    Cat cat2;
+    Cat2 cat2;
 }
