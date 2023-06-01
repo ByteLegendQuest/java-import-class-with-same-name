7a915dfeae005603c9fd4081ff1aeeaf94124c72
DejanPerovic <dejanp0@gmail.com>
2023-06-01 10:58:36 +0000

    Update at 2023-06-01T10:58:36.940052Z

--- src/main/java/com/bytelegend/Home.java
+++ src/main/java/com/bytelegend/Home.java
@@ -1,11 +1,6 @@
 package com.bytelegend;
-package com.bytelegend.pet1;
-package com.bytelegend.pet2;
-
-import com.bytelegend.pet1.Cat;
-import com.bytelegend.pet2.Cat;
 
 public class Home {
-    Cat cat1;
-    Cat cat2;
+    com.bytelegend.pet1.Cat cat1;
+    com.bytelegend.pet2.Cat cat2;
 }
