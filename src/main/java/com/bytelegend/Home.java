fca204a5b3f8a2da387f7d54c56ee90d0b888ac2
cai895015380 <65594155+cai895015380@users.noreply.github.com>
2023-07-03 18:07:34 +0800

    Update Home.java

--- src/main/java/com/bytelegend/Home.java
+++ src/main/java/com/bytelegend/Home.java
@@ -1,12 +1,9 @@
 package com.bytelegend;
 
-import com.bytelegend.pet1.Cat as Cat1;
-import com.bytelegend.pet2.Cat as Cat2;
 
 public class Home {
 
     com.bytelegend.pet1.Cat cat1 = new com.bytelegend.pet1.Cat();
     com.bytelegend.pet2.Cat cat2 = new com.bytelegend.pet2.Cat();
-
     
 }
