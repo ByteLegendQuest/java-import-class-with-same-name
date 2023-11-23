3254509055737c6db18e42219b2d9a56855212d7
Eloi0424 <beichu0424@qq.com>
2023-11-23 15:17:57 +0800

    import class with same name complete fix

--- src/main/java/com/bytelegend/pet1/Cat.java
+++ src/main/java/com/bytelegend/pet1/Cat.java
@@ -1,7 +1,3 @@
 package com.bytelegend.pet1;
 
-public class Cat {
-    public Cat(){
-        System.out.println("I am a cat from pet1");
-    }
-}
+public class Cat {}
--- src/main/java/com/bytelegend/pet2/Cat.java
+++ src/main/java/com/bytelegend/pet2/Cat.java
@@ -1,7 +1,3 @@
 package com.bytelegend.pet2;
 
-public class Cat {
-    public Cat(){
-        System.out.println("I am a cat from pet2");
-    }
-}
+public class Cat {}
